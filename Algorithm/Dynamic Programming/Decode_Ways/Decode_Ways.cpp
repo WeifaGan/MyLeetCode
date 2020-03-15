7db732d5232a6177bc2381277a98eb12063183e0
WeifaGan <44939038+WeifaGan@users.noreply.github.com>
2020-03-15 17:22:37 +0800

    Update Decode_Ways.cpp

--- Algorithm/Dynamic Programming/Decode_Ways/Decode_Ways.cpp
+++ Algorithm/Dynamic Programming/Decode_Ways/Decode_Ways.cpp
@@ -7,12 +7,12 @@ public:
 		v[1] = v[0] = 1;
 		for (int i = 2; i<len+1; i++)
 		{
-			if (s[i-1] != '0')v[i] = v[i - 1];
+			if (s[i-1] != '0'){v[i] = v[i - 1];}
 			else
 			{
 				v[i] = 0;
 			}
-        int value=s[i-1]-'0';
+        		int value=s[i-1]-'0';
 			if (s[i - 2] == '1' || s[i - 2]=='2'&&value<=6)
 			{
 				v[i] += v[i - 2];
