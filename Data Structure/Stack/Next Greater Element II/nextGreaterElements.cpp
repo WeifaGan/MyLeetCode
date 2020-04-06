89b9c87f85f8d1cdad8e828d3097ede39ad73c7d
WeifaGan <44939038+WeifaGan@users.noreply.github.com>
2020-04-06 20:10:20 +0800

    Update nextGreaterElements.cpp

--- Data Structure/Stack/Next Greater Element II/nextGreaterElements.cpp
+++ Data Structure/Stack/Next Greater Element II/nextGreaterElements.cpp
@@ -24,3 +24,28 @@ public:
         
     }
 };
+
+********or**************
+
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n=nums.size();
+        stack<int>mystack;
+        vector<int>result(n);
+        int j;
+        for(int i=n+n-1;i>=0;i--)
+        {
+ 
+            while(!mystack.empty()&&nums[i%n]>=mystack.top())
+            {
+                mystack.pop();
+            }
+            
+            result[i%n]=mystack.empty()?-1:mystack.top();
+            mystack.push(nums[i%n]);            
+        }
+        return result;
+        
+    }
+};
